afdd46d05e0e5acdad5ceefec69a5a65b0b201af
Ahlamalmutiri <Ahlamessaalmutiri.it@gmail.com>
2022-11-02 06:19:32 -0700

    Initial Commit

---
+++ Adventure/AdventureApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdventureApp.swift
+//  Adventure
+//
+//  Created by AHLAM on 08/04/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct AdventureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
