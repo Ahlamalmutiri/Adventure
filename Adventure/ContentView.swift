b48c008f12e23807e24bf71a855264f0f9a72cf7
Ahlamalmutiri <117260736+Ahlamalmutiri@users.noreply.github.com>
2022-11-02 06:59:20 -0700

    Update ContentView.swift

--- Adventure/ContentView.swift
+++ Adventure/ContentView.swift
@@ -9,15 +9,48 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
-}
+        NavigationView{
+            
+            VStack(spacing: 50){
+                Text("    Explore Your \nFavorite Journey")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .foregroundColor(Color("TextOne"))
+                
+                Image("Start")
+                    .resizable()
+                    .frame(width: 450 , height: 450)
+                
+                NavigationLink{
+                    Homepage()
+                }label: {
+                    Text("Get Started ")
+                        .font(.title)
+                        .fontWeight(.semibold)
+                        .padding(.vertical,18)
+                        .frame(maxWidth: .infinity)
+                        .background(Color("TextTwo"))
+                        .cornerRadius(60)
+                        .shadow(color:Color.black.opacity(0.1),radius: 5,x:5,y:5)
+                    
+                        .foregroundColor(.white)
+                        .padding(.horizontal,80)
+                }
+                
+              
+                   
+              
+            }}.navigationBarBackButtonHidden(true)
+        
+    }}
+                
+                
+                
+                
+                
+        
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
